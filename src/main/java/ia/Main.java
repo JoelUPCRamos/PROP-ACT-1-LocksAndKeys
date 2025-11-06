ad217a93133f57e8e5877a169c49b1f1da554bd9
Joel Ramos Mejías <joel.ramos@estudiantat.upc.edu>
2025-11-06 19:13:57 +0100

    Estructura inicial del projecte i fitxers base

---
+++ src/main/java/ia/Main.java
@@ -0,0 +1,8 @@
+package ia;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Activitat 1 - Locks & Keys");
+        System.out.println("Repositori inicial correctament configurat");
+    }
+}
